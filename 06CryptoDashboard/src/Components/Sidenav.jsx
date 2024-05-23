258e2aa6f583a9c450c66c261558a4726562b810
sujitsk2912 <skhangar321@gmail.com>
2024-05-23 10:30:29 +0530

    Crypto Dashboard Completed

--- 06CryptoDashboard/src/Components/Sidenav.jsx
+++ 06CryptoDashboard/src/Components/Sidenav.jsx
@@ -32,12 +32,13 @@ const Sidenav = () => {
           </Link>
         ))}
       </div>
-      <div className="w-full flex items-center rounded-md shadow-sm justify-start gap-4 hover:bg-[#F3F3F7] mb-3 px-4 py-3 font-[500] text-gray-500 transition-all hover:text-black hover:cursor-pointer">
-        <BiSupport />
-        <Link to="/support">
+      <Link to="/support" className="w-full">
+        <div className="w-full flex items-center rounded-md shadow-sm justify-start gap-4 hover:bg-[#F3F3F7] mb-3 px-4 py-3 font-[500] text-gray-500 transition-all hover:text-black hover:cursor-pointer">
+          <BiSupport />
+
           <h4 className="text-[15px]">Support</h4>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </aside>
   );
 };
