4dd555ebd501dfdc0692a83cf9b5645fcde78396
sujitsk2912 <skhangar321@gmail.com>
2024-05-29 13:13:38 +0530

    update

--- 05FirebaseContactApp/src/Components/FirebaseHeader.jsx
+++ 05FirebaseContactApp/src/Components/FirebaseHeader.jsx
@@ -1,7 +1,7 @@
 const FirebaseHeader = () => {
   return (
     <header className="bg-white m-auto text-black flex items-center justify-center gap-3 p-4 max-w-7xl rounded-lg">
-      <img src="/public/images/logos_firebase.svg" alt="firebase logo" />
+      <img src="../images/logos_firebase.svg" alt="firebase logo" />
       <h2 className="font-semibold text-lg">Firebase Contact App</h2>
     </header>
   );
